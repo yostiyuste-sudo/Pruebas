27763e1b9956443ec44991708ade926da2b5effe
proyectos paula diaz <pauladiaz0713@gmail.com>
2026-03-03 14:06:10 -0500

    cambio x

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Rol, Usuario, TipoIdentificacion, TipoContacto, 
+    TipoInteraccion, Contacto, Interaccion, Compromiso
+)
 
-# Register your models here.
+# Registro básico de tus modelos
+admin.site.register(Rol)
+admin.site.register(Usuario)
+admin.site.register(TipoIdentificacion)
+admin.site.register(TipoContacto)
+admin.site.register(TipoInteraccion)
+admin.site.register(Contacto)
+admin.site.register(Interaccion)
+admin.site.register(Compromiso)
