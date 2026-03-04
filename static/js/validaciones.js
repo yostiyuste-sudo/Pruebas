9290d42f1dbf5aa72f9b12e748c05b505b2935d8
proyectos paula diaz <pauladiaz0713@gmail.com>
2026-03-03 21:19:04 -0500

    login

---
+++ static/js/validaciones.js
@@ -0,0 +1,12 @@
+function validarEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validarNumeros(valor) {
+    return /^\d+$/.test(valor);
+}
+
+function validarCampoRequerido(valor) {
+    return valor.trim().length > 0;
+}
