15f8805e7c9f3d0a7dde91d89eb85c2f3875d4b7
Jaary luna <jaryluna07@gmail.com>
2026-03-20 14:13:06 -0500

    models

--- core/models.py
+++ core/models.py
@@ -7,11 +7,15 @@ class Rol(models.Model):
         return self.nombre_rol
 
 class Usuario(models.Model):
-    id_rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    # Relación con el modelo Rol
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE) 
     nombre_usuario = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password_hash = models.CharField(max_length=255)
     activo = models.BooleanField(default=True)
+    
+    # --- CAMPO AGREGADO AQUÍ ---
+    token_verificacion = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.nombre_usuario
