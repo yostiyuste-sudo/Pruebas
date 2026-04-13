15f220500288631285b7dda9de26d86385e3090d
yostiyuste-sudo <yostiyuste@gmail.com>
2026-04-12 19:59:21 -0500

    solucion

--- config/settings.py
+++ config/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_dyco',
-        'USER': 'yosti',
-        'PASSWORD': '2634',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Password validation
